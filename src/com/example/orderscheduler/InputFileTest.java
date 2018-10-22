6242c5baa8e628bf0b02b03c6fb927ae6deadb0a
HarshitaMeena <harshita1805@gmail.com>
2018-10-21 20:44:56 -0500

    Added input file

--- src/com/example/orderscheduler/InputFileTest.java
+++ src/com/example/orderscheduler/InputFileTest.java
@@ -26,11 +26,6 @@ public class InputFileTest {
 
             StrategyBasic thewarehouse = new StrategyBasic(server, drone, 6*3600, 22*3600);
             thewarehouse.startProcessingOrder();
-            assert thewarehouse.getCompleteOrders().get(0).getOrderId().equals("WM002") : "Valid order1";
-            assert thewarehouse.getCompleteOrders().get(1).getOrderId().equals("WM001") : "Valid order2";
-            assert thewarehouse.getCompleteOrders().get(2).getOrderId().equals("WM004") : "Valid order3";
-            assert thewarehouse.getCompleteOrders().get(3).getOrderId().equals("WM003")  : "Valid order4";
-            assert thewarehouse.getNPSValue() == 75.0 : "NPSValue";
 
             try (FileWriter fw = new FileWriter("output.txt")) {
 
